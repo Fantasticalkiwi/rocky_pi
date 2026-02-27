4868a9de23be08d67e2d9ae4e6e73c389b7bdfd9
Kai-Xing Yue <79027796+Fantasticalkiwi@users.noreply.github.com>
2026-02-27 11:21:33 -0500

    Create Rocky_Motor_parametization.m

---
+++ Rocky_Motor_parametization.m
@@ -0,0 +1,62 @@
+%loads and plots the motor calibration data
+function [t, y_L, v_L, y_R, v_R] = load_motor_data()
+%path and file name of data
+fpath = 'C:\Users\taylorott\Documents\ESA_Data\'; %path (change this!)
+fname_in = 'motor_calibration_data.txt'; %file name (change this!)
+%load the motor calibration data
+motor_data = importdata([fpath,fname_in]);
+%unpack the motor calibration data
+t = motor_data(:,1)./1000;
+y_L = motor_data(:,2); v_L = motor_data(:,3)./100;
+y_R = motor_data(:,4); v_R = motor_data(:,5)./100;
+%plot the motor calibration data
+figure(1);
+subplot(2,1,1);
+hold on
+plot(t,v_L,'k','linewidth',1);
+plot(t,v_R,'r','linewidth',1);
+xlabel('time (sec)'); ylabel('wheel speed (m/sec)');
+title('Motor Calibration Data');
+h1 = legend('Left Wheel','Right Wheel');
+set(h1,'location','southeast');
+subplot(2,1,2);
+hold on
+plot(t,y_L,'k','linewidth',1);
+plot(t,y_R,'r--','linewidth',1);
+xlabel('time (sec)'); ylabel('wheel command (-)');
+title('Motor Calibration Data');
+h2 = legend('Left Wheel','Right Wheel');
+set(h2,'location','southeast');
+
+
+a_g = 1/.3; c_g = 1;
+%run the fit.
+%List of guesses should be in alphabetical order (or parameters)
+f_params = fit(t,v_L,'c*(1-exp(-a*x))','StartPoint',[a_g,c_g]);
+%unpack the result
+a = f_params.a; c = f_params.c;
+%Evaluate the fit function at the data points
+vL_fit = c*(1-exp(-a*t));
+%compare fit with the data
+hold on
+plot(t,v_L,'ro','markerfacecolor','r','markersize',2);
+plot(t,vL_fit,'k','linewidth',2);
+xlabel('t (s)'); ylabel('v_L (m/s)'); title('Fit Motor Data Left Wheel');
+legend('Data','Fit');
+
+a_g = 1/.3; c_g = 1;
+%run the fit.
+%List of guesses should be in alphabetical order (or parameters)
+f_params = fit(t,v_R,'c*(1-exp(-a*x))','StartPoint',[a_g,c_g]);
+%unpack the result
+a = f_params.a; c = f_params.c;
+%Evaluate the fit function at the data points
+vR_fit = c*(1-exp(-a*t));
+%compare fit with the data
+hold on
+plot(t,v_R,'ro','markerfacecolor','r','markersize',2);
+plot(t,vR_fit,'k','linewidth',2);
+xlabel('t (s)'); ylabel('v_L (m/s)'); title('Fit Motor Data Left Wheel');
+legend('Data','Fit');
+
+end
